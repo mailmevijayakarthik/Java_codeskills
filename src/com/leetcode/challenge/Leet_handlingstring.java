c85cddf2dcf0917a83f000b25a28d0b62ba4930c
Vijayakarthikeyan Arul <vijayakarthikeyan.arul@heb.com>
2019-10-21 22:12:49 -0500

    Added Leet_handlingstring file

--- src/com/leetcode/challenge/Leet_handlingstring.java
+++ src/com/leetcode/challenge/Leet_handlingstring.java
@@ -1,34 +1,54 @@
 package com.leetcode.challenge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leet_handlingstring {
-	public  void main(String arg[]) {
+	public  static void main(String arg[]) {
 		String s="krmyfshbspcgtesxnnljhfursyissjnsocgdhgfxubewllxzqhpasguvlrxtkgatzfybprfmmfithphckksnvjkcvnsqgsgosfxc";
 		int k = 20;
-		String newReverse="";
-		reverseStr(s, k);
+		
+		String needed = reverseStr(s, k);
+		System.out.println(needed);
 	
 		}
 
-	private void reverseStr(String s, int k) {
-		// TODO Auto-generated method stub
+	private static String reverseStr(String s, int k) {
+		
+		String neededrev="";
 		if(s.toCharArray().length>2*k) {
 			String mysplit=s.substring(0, 2*k);
 			String leftover=s.substring(2*k);
+			neededrev=replace_n_characters(mysplit,k);
 			
-			String newReverse = replace_n_characters(mysplit,k);
-			s=leftover;
-			reverseStr(s,k);
+		//	s=leftover;
+		//	reverseStr(s,k);
+			while(leftover.toCharArray().length>0) {
 			
+			if(leftover.toCharArray().length>2*k) {
+				String leftsplit1=leftover.substring(0, 2*k);
+				String leftsplitrest=leftover.substring(2*k);
+				neededrev=neededrev+replace_n_characters(leftsplit1,k);
+			}else if(leftover.toCharArray().length>k && leftover.toCharArray().length<2*k) {
+				neededrev=neededrev+replace_n_characters(leftover,k);
+			}else if(leftover.toCharArray().length<k) {
+				neededrev=neededrev+reversestring(leftover);
+			}
+			}
 			
 		}else if(s.toCharArray().length>k && s.toCharArray().length<2*k ) {
-			
+			String split1=s.substring(0, k);
+			String splitrest=s.substring(k);
+			neededrev=neededrev+reversestring(split1)+splitrest;
 			
 		}else if(s.toCharArray().length<k) {
-			
+			neededrev=neededrev+reversestring(s);
 		}
+		
+		return neededrev;
 	}
 
-	private  String replace_n_characters(String mysplit, int k) {
+	private static String replace_n_characters(String mysplit, int k) {
 		// TODO Auto-generated method stub
 		String first = mysplit.substring(0, k);
 		String remaining = mysplit.substring(k);
@@ -37,7 +57,7 @@ public class Leet_handlingstring {
 		return mysplit;
 	}
 	
-	private String reversestring(String reverse) {
+	private static String reversestring(String reverse) {
 		String rev = "";
 		char[] listchar = reverse.toCharArray();
 		for(int i=listchar.length-1;i>=0;i--) {
